b3774d48ab3e251bdb2ba0a6145eb104939f5d91
brotherlogic <brotherlogic@gmail.com>
2017-04-24 20:32:01 -0700

    Added test of running jobs. This closes #3.

--- master.go
+++ master.go
@@ -1,9 +1,21 @@
 package main
 
 import pb "github.com/brotherlogic/gobuildmaster/proto"
+import pbs "github.com/brotherlogic/gobuildslave/proto"
 
 func configDiff(cm, cs *pb.Config) *pb.Config {
 	return cm
+}
+
+func runJobs(c *pb.Config) []*pbs.JobSpec {
+	var jobs []*pbs.JobSpec
+	for _, j := range c.Intents {
+		for i := 0; i < int(j.Masters); i++ {
+			jobs = append(jobs, j.Spec)
+		}
+	}
+
+	return jobs
 }
 
 func blank() {
