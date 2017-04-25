039acef7c0a52e44af9a75a9ba8ca1160f697891
brotherlogic <brotherlogic@gmail.com>
2017-04-24 20:01:08 -0700

    Added config spec

--- master.go
+++ master.go
@@ -1,5 +1,11 @@
 package main
 
+import pb "github.com/brotherlogic/gobuildmaster/proto"
+
+func configDiff(cm, cs *pb.Config) *pb.Config {
+	return cm
+}
+
 func blank() {
 
 }
--- master_test.go
+++ master_test.go
@@ -1,6 +1,19 @@
 package main
 
 import "testing"
+import pb "github.com/brotherlogic/gobuildmaster/proto"
+import pbs "github.com/brotherlogic/gobuildslave/proto"
+
+func TestDiff(t *testing.T) {
+	i1 := &pb.Intent{Spec: &pbs.JobSpec{Name: "testing"}}
+	c1 := &pb.Config{Intents: []*pb.Intent{i1}}
+	c2 := &pb.Config{Intents: []*pb.Intent{}}
+
+	diff := configDiff(c1, c2)
+	if len(diff.Intents) != 1 || diff.Intents[0].Spec.Name != "testing" {
+		t.Errorf("Error in diff: %v", diff)
+	}
+}
 
 func TestClean(t *testing.T) {
 	blank()
