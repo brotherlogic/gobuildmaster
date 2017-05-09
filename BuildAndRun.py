75945f2669bc628d9ed4a82d359eceba016cdf11
Simon Tucker <brotherlogic@gmail.com>
2017-05-08 17:24:05 -0700

    Updated build and run script

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -24,6 +24,8 @@ running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
 
               
 if size_1 != size_2 or new_hash != current_hash or not running:
+    for line in os.popen('cp out.txt out-' + `time.time()`).readlines():
+        pass
     for line in os.popen('killall ' + name).readlines():
         pass
     subprocess.Popen(['./' + name])
