6b01cdc18fce9277991fc5628f1d6c904ad47ee6
Simon Tucker <brotherlogic@gmail.com>
2017-08-09 11:37:27 -0700

    Fix running detection for master

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -20,7 +20,11 @@ for line in os.popen('go build').readlines():
 size_1 = os.path.getsize('./old' + name)
 size_2 = os.path.getsize('./' + name)
 
-running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
+lines = os.popen('ps -ef | grep ' + name).readlines()
+running = False
+for line in lines:
+    if "./" + name in line:
+        running = True
 
               
 if size_1 != size_2 or new_hash != current_hash or not running:
