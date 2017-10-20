748ea7b4ee421d00b121cf0145823a36e1c996ea
Simon Tucker <brotherlogic@gmail.com>
2017-10-19 20:36:09 -0700

    Turn off quiet override

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -35,4 +35,4 @@ if size_1 != size_2 or new_hash != current_hash or not running:
         pass
     for line in os.popen('killall ' + name).readlines():
         pass
-    subprocess.Popen(['./' + name, "--quiet=false"])
+    subprocess.Popen(['./' + name])
