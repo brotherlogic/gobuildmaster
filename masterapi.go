d016675523826bbf9f636b0c0bff272c979e7dcb
Simon Tucker <simon@brotherlogic@gmail>
2019-11-22 04:58:35 -0800

    Move to v2

--- masterapi.go
+++ masterapi.go
@@ -531,7 +531,7 @@ func main() {
 	s.Register = s
 	s.PrepServer()
 	s.GoServer.Killme = false
-	err = s.RegisterServer("gobuildmaster", false)
+	err = s.RegisterServerV2("gobuildmaster", false)
 	if err != nil {
 		log.Fatalf("Unable to register: %v", err)
 	}
